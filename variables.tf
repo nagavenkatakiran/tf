aba4d274359c93af914a94e6a483736d63f6f0b5
nagavenkatakiran <nagavenkatakiran@pec.edu>
2018-12-14 17:45:31 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,13 @@
+# AWS Config
+
+variable "aws_access_key" {
+  default = "AKIAJW4HAPGGUEWDEATQ"
+}
+
+variable "aws_secret_key" {
+  default = "gSwkVU8mzSZwb6dJxXKpOPqv31vI9L+Q+NtyceoM"
+}
+
+variable "aws_region" {
+  default = "us-east-1"
+}
