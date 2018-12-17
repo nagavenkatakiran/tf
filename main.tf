b412d73e099235709dd4bcbc6c5826c9d56b35e4
nagavenkatakiran <nagavenkatakiran@pec.edu>
2018-12-17 17:43:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my-test-instance" {
   ami             = "${data.aws_ami.ubuntu.id}"
   instance_type   = "t2.micro"
-
+key_name = "helloworld"
   tags {
     Name = "test-instance3"
   }
