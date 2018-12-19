1cf9ce49d90a9d8cf613c6e179487216f09c37c6
nagavenkatakiran <nagavenkatakiran@pec.edu>
2018-12-19 15:50:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ resource "aws_instance" "my-test-instance" {
  
  
   tags {
-    Name = "test-instance66"
+    Name = "Dec19"
   }
   
   
-  resource "aws_key_pair" "terraform_ec2_key" {
-   key_name = "terraform_ec2_key"
+  resource "aws_key_pair" "helloworld" {
+   key_name = "helloworld"
 public_key= "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsXAhWPJEayjt7ODRP4+isaEGNMGnyfb9a5APyXqXqzaXQLFmqjDfsqiNkwEGLUQzejQKAaoVtcPPKhdvQM0c7ZwzOvQYZw9Q9I5baAVRE22fMj0MpY+Ff3EmpRxxgu0Y/WWzHDS+Gv/Nr+eLZLmSgou3SmR5hhSFXUfssCNQe+/oKUT56ZZDivKm4bl/etRlkA4U/qzYkgEw5EvxAMjcGSmqqZnnYpdvXXyTXEntOXzzCmyIFuUbSgnesUCKg6LyA2hHzww3GAmrqdF7nxlj+EAfOI2K+POLCaLiSW8bCQLhmp9EWqpXyyk7ZpR9RSVPUCYWCtMmEPJHhEksuyCnL ubuntu@ip-172-31-94-100"
 
 
