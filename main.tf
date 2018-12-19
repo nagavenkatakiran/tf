7a8c7594187c916514608839760f735800b56b96
nagavenkatakiran <nagavenkatakiran@pec.edu>
2018-12-19 15:53:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "my-test-instance" {
   
   resource "aws_key_pair" "helloworld" {
    key_name = "helloworld"
-public_key= "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsXAhWPJEayjt7ODRP4+isaEGNMGnyfb9a5APyXqXqzaXQLFmqjDfsqiNkwEGLUQzejQKAaoVtcPPKhdvQM0c7ZwzOvQYZw9Q9I5baAVRE22fMj0MpY+Ff3EmpRxxgu0Y/WWzHDS+Gv/Nr+eLZLmSgou3SmR5hhSFXUfssCNQe+/oKUT56ZZDivKm4bl/etRlkA4U/qzYkgEw5EvxAMjcGSmqqZnnYpdvXXyTXEntOXzzCmyIFuUbSgnesUCKg6LyA2hHzww3GAmrqdF7nxlj+EAfOI2K+POLCaLiSW8bCQLhmp9EWqpXyyk7ZpR9RSVPUCYWCtMmEPJHhEksuyCnL ubuntu@ip-172-31-94-100"
+# public_key= "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCsXAhWPJEayjt7ODRP4+isaEGNMGnyfb9a5APyXqXqzaXQLFmqjDfsqiNkwEGLUQzejQKAaoVtcPPKhdvQM0c7ZwzOvQYZw9Q9I5baAVRE22fMj0MpY+Ff3EmpRxxgu0Y/WWzHDS+Gv/Nr+eLZLmSgou3SmR5hhSFXUfssCNQe+/oKUT56ZZDivKm4bl/etRlkA4U/qzYkgEw5EvxAMjcGSmqqZnnYpdvXXyTXEntOXzzCmyIFuUbSgnesUCKg6LyA2hHzww3GAmrqdF7nxlj+EAfOI2K+POLCaLiSW8bCQLhmp9EWqpXyyk7ZpR9RSVPUCYWCtMmEPJHhEksuyCnL ubuntu@ip-172-31-94-100"
 
 
 }
