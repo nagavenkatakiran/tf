6760de154fdee73788de5d27768a7286d1ef6587
nagavenkatakiran <nagavenkatakiran@pec.edu>
2018-12-26 17:42:53 +0530

    Add files via upload

---
+++ fortom.sh
@@ -0,0 +1,4 @@
+wget http://mirrors.wuchna.com/apachemirror/tomcat/tomcat-8/v8.5.37/bin/apache-tomcat-8.5.37.tar.gz;
+tar xvzf apache-tomcat-8.5.37.tar.gz;
+cd /home/ubuntu/apache-tomcat-8.5.37/bin;
+sh startup.sh
